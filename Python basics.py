2e6429cd8799a5b9d614b2c76bc56dd0cec4d461
Sai Khairnar <85782407+saikhairnar20023@users.noreply.github.com>
2023-01-24 16:32:48 +0530

    Add files via upload

---
+++ Python basics.py
@@ -0,0 +1,174 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+print("hello")
+
+
+# In[4]:
+
+
+a=10
+g=5.5
+t="hello everyone"
+k='p'
+print(t)
+
+
+# In[6]:
+
+
+print("hello\nwelcome to python")
+
+
+# In[8]:
+
+
+int(5.5)
+
+
+# In[10]:
+
+
+float(6)
+
+
+# In[12]:
+
+
+str(4)
+
+
+# In[15]:
+
+
+num1=(10)
+num2=(20)
+num3= num1+num2
+print(num3)
+
+
+# In[18]:
+
+
+num3= num2-num1
+print(num3)
+
+
+# In[20]:
+
+
+num3= num1*num2
+print(num3)
+
+
+# In[23]:
+
+
+type(2)
+
+
+# In[25]:
+
+
+type(3.9)
+
+
+# In[27]:
+
+
+type('hello')
+
+
+# In[67]:
+
+
+a=10
+print("%F"%a)
+
+
+# In[36]:
+
+
+b="hello everyone"
+print("b=%s"%b)
+
+
+# In[38]:
+
+
+5==5
+
+
+# In[40]:
+
+
+5>8
+
+
+# In[44]:
+
+
+8<4
+
+
+# In[46]:
+
+
+5==5 and 5>4
+
+
+# In[48]:
+
+
+6==5 or 9<7
+
+
+# In[51]:
+
+
+7<9
+
+
+# In[55]:
+
+
+6!=5 or 8<4
+
+
+# In[58]:
+
+
+x=10
+y=5
+x=x+y
+x
+
+
+# In[61]:
+
+
+y=x-y
+y
+
+
+# In[63]:
+
+
+x=x-y
+x
+
+
+# In[66]:
+
+
+print("x=%d y=%d"%(x,y))
+
+
+# In[ ]:
+
+
+
+
