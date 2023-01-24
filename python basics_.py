3d8d77b2f079e2aad70759b393c01dadb9cbef15
Sai Khairnar <85782407+saikhairnar20023@users.noreply.github.com>
2023-01-24 16:52:55 +0530

    Add files via upload

---
+++ python basics_.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 24 16:34:39 2023
+
+@author: Sai khairnar
+"""
+#length keyword
+len ("abcdefghij")
+
+#find keyword use to find perticular index location of string 
+s="abcdefghijklmnop"
+s.find("def")
+
+#anoter len exmaple
+len("japan")
+
+s2="swing"
+s2.find("wing")
+
+#extracting of string
+s="abcdefghijklmnop"
+s[5:9]
+# after colon end index
+s[:5]
+
+#before colon represent start index
+s[3:] 
+
+#replacing the index
+s="wait wait untill you get succeed"
+s.replace("wait", "work")
+
+#converting upper string to lower
+c="hello world"
+c.upper()
+c.lower()
+
